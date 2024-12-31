473e03a483db893698c945bf81b6ee063550771f
Mish0ka0 <sega.vasilev.2014@mail.ru>
2024-12-31 11:48:47 +0300

    модуль 13, бот

--- module 13- Основы асинхронного программирования на базе фреймворка aiogram/module_13_5.py
+++ module 13- Основы асинхронного программирования на базе фреймворка aiogram/module_13_5.py
@@ -1,12 +1,18 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.dispatcher import FSMContext
 import asyncio
 
 api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button_1 = KeyboardButton(text='Рассчитать')
+button_2 = KeyboardButton(text='Информация')
+kb.add(button_1, button_2)
 
 
 class UserState(StatesGroup):
@@ -15,7 +21,12 @@ class UserState(StatesGroup):
     weight = State()
 
 
-@dp.message_handler(text='Calories')
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=kb)
+
+
+@dp.message_handler(text='Рассчитать')
 async def set_age(message):
     await message.answer('Введите свой возраст:')
     await UserState.age.set()
@@ -42,6 +53,11 @@ async def send_calories(message, state):
     calories = 10 * float(data['weight']) + 6.25 * float(data['growth']) - 5 * int(data['age']) + 5
     await message.answer(f'Ваша норма калорий: {calories}')
     await state.finish()
+
+
+@dp.message_handler()
+async def all_messages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
